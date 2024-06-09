16e1f36334a9104772384f90b0103c85b9106af7
MARVIN <o.silverygold@gmail.com>
2024-06-09 17:42:52 +0300

    Add book to reading list function

--- frontend/ello-book-assignment/src/components/Search/SearchResult.jsx
+++ frontend/ello-book-assignment/src/components/Search/SearchResult.jsx
@@ -6,12 +6,26 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import PlaylistAddCircleIcon from '@mui/icons-material/PlaylistAddCircle';
+import DoneIcon from '@mui/icons-material/Done';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBook } from '../../redux/readingList/readingListSlice';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function SearchResult({ book }) {
   const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+  const readingList = useSelector(state => state.readingList.bookList)
+  const dispatch = useDispatch();
+
+  const handleAddBookToList = () => {
+    dispatch(addBook(book))
+    toast.success(`${book.title} added to reading list.`);
+  }
+
+  const isBookInReadingList = readingList.find(item => item.title === book.title);
 
   return (
     <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
@@ -39,8 +53,11 @@ export default function SearchResult({ book }) {
             </React.Fragment>
           }
         />
-        <ListItemIcon>
-          <PlaylistAddCircleIcon />
+        <ListItemIcon 
+          onClick={isBookInReadingList ? undefined : handleAddBookToList} 
+          sx={{ cursor: 'pointer' }}
+        >
+          {isBookInReadingList ? <DoneIcon style={{ color: 'green', fontSize:"24" }} /> : <PlaylistAddCircleIcon />}
         </ListItemIcon>
       </ListItem>
       <Divider variant="inset" component="li" />
